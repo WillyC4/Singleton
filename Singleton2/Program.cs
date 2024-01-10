b78fd15f5e3bda7b0bc53dc12266cb5067ee53b4
wilyc <wilyc@SATELLITE>
2024-01-10 09:57:40 -0500

    Agregar archivos de proyecto.

---
+++ Singleton2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = Singleton.Log.Instance;
+            log.Save("a");
+            log.Save("b");
+
+            var a = Singleton.Singleton.Instance;
+            var b = Singleton.Singleton.Instance;
+
+            Console.WriteLine(a == b);
+        }
+    }
+}
